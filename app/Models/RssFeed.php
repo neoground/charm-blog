35050145efa7ce42a551e558774856ca59c62427
Sven Reifschneider <sven@neoground.com>
2025-06-07 21:27:50 +0200

    🔧 Improve RSS feed creator to support custom RSS feeds (besides the blog one)

--- app/Models/RssFeed.php
+++ app/Models/RssFeed.php
@@ -18,7 +18,7 @@ use Charm\Vivid\C;
 class RssFeed
 {
     /**
-     * Create and save an XML feed for a specified language.
+     * Create and save a blog XML feed for a specified language.
      *
      * @param string $lang The language code for which to create the feed or 'all' for all
      *
@@ -27,6 +27,7 @@ class RssFeed
     public static function createXml(string $lang): bool|int
     {
         $feed_path = C::Storage()->getDataPath() . DS . 'blog' . DS . 'feed_' . $lang . '.xml';
+        C::Storage()->createDirectoriesIfNotExisting($feed_path);
         C::Storage()->deleteFileIfExists($feed_path);
 
         $b = new BlogPost();
@@ -38,52 +39,116 @@ class RssFeed
         // Limit to latest 50 posts
         $posts = array_slice($posts, 0, C::Config()->get('blog:rss.amount', 50));
 
-        // XML header
-        $content = "<?xml version='1.0' encoding='UTF-8'?>\n<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">\n<channel>\n
-<title>" . C::Config()->get('blog:rss.title_' . $lang) . "</title>
-<link>" . C::Config()->get('blog:rss.link_' . $lang) . "</link>
-<description>" . C::Config()->get('blog:rss.description_' . $lang) . "</description>
-<generator>" . C::Config()->get('blog:rss.generator') . "</generator>
-<copyright>" . C::Config()->get('blog:rss.copyright') . "</copyright>
-<image>
-  <link>" . C::Config()->get('blog:rss.blog_base_url') . "</link>
-  <title>" . C::Config()->get('blog:rss.title_' . $lang) . "</title>
-  <url>" . C::Router()->getBaseUrl() . "/" . C::Config()->get('blog:rss.image_relpath') . "</url>
-</image>
-<lastBuildDate>" . Carbon::now()->toRfc2822String() . "</lastBuildDate>
-<atom:link href=\"" . C::Config()->get('blog:rss.blog_base_url') . "/feed/" . $lang . "\" rel=\"self\" type=\"application/rss+xml\" />
-<language>" . $lang . "</language>\n\n";
-
+        // Format posts in the right array
+        $all_langs = C::Config()->get('main:session.available_languages', []);
+        $postarr = [];
         foreach ($posts as $post) {
             if ($post['is_published']) {
                 // Add single item (post)
                 if ($lang != 'all') {
                     // Single language
-                    $content .= self::getEntryXml($post, '_' . $lang);
+                    $postarr[] = self::formatBlogPostToEntry($post, '_' . $lang);
                 } else {
                     // All languages
-                    foreach (C::Config()->get('main:session.available_languages', []) as $l) {
-                        $content .= self::getEntryXml($post, '_' . $l);
+                    foreach ($all_langs as $l) {
+                        $postarr[] = self::formatBlogPostToEntry($post, '_' . $l);
                     }
                 }
+            }
+        }
+
+        // XML
+        $title = C::Config()->get('blog:rss.title_' . $lang);
+        $description = C::Config()->get('blog:rss.description_' . $lang);
+        $blog_url = C::Config()->get('blog:rss.link_' . $lang);
+        $feed_url = C::Config()->get('blog:rss.blog_base_url') . "/feed/" . $lang;
+        $content = self::createCustomXmlFeed($postarr, $title, $description, $blog_url, $feed_url, $lang);
+
+        return file_put_contents($feed_path, $content);
+    }
+
+    /**
+     * Create a custom XML feed.
+     *
+     * @param array  $posts       the posts array. each sub-array must have a structure suitable for
+     *                            self::getEntryXml().
+     * @param string $title       RSS / blog title.
+     * @param string $description RSS / blog description.
+     * @param string $blog_url    absolute URL to the blog / area the RSS feed is about.
+     * @param string $feed_url    absolute URL to the feed.
+     * @param string $language    language of the feed (e.g., de, en).
+     *
+     * @return string the RSS feed as a formatted XML string.
+     */
+    public static function createCustomXmlFeed(array  $posts, string $title, string $description,
+                                               string $blog_url, string $feed_url, string $language = 'en'): string
+    {
+        // XML header
+        $content = "<?xml version='1.0' encoding='UTF-8'?>\n<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">\n<channel>\n
+<title>" . $title . "</title>
+<link>" . $blog_url . "</link>
+<description>" . $description . "</description>
+<generator>" . C::Config()->get('blog:rss.generator', 'charm-blog') . "</generator>
+<copyright>" . C::Config()->get('blog:rss.copyright', '') . "</copyright>
+<image>
+  <link>" . $blog_url . "</link>
+  <title>" . $title . "</title>
+  <url>" . C::Router()->getBaseUrl() . "/" . C::Config()->get('blog:rss.image_relpath') . "</url>
+</image>
+<lastBuildDate>" . Carbon::now()->toRfc2822String() . "</lastBuildDate>
+<atom:link href=\"" . $feed_url . "\" rel=\"self\" type=\"application/rss+xml\" />
+<language>" . $language . "</language>\n\n";
+
+        foreach ($posts as $post) {
+            if (!array_key_exists('is_published', $post) || $post['is_published']) {
+                $content .= self::getEntryXml($post);
             }
         }
 
         // XML footer
         $content .= "\n</channel>\n</rss>";
 
-        return file_put_contents($feed_path, $content);
+        return $content;
     }
 
-    private static function getEntryXml(array $post, string $key_suffix = null): string
+    /**
+     * Format a blog post array into an entry compatible with self::getEntryXml().
+     *
+     * @param array       $post       the blog post array.
+     * @param string|null $key_suffix optional key suffix (e.g., language).
+     *
+     * @return array the array in the right structure.
+     */
+    private static function formatBlogPostToEntry(array $post, string $key_suffix = null): array
+    {
+        return [
+            'title' => $post['title' . $key_suffix],
+            'link' => C::Config()->get('blog:rss.blog_base_url') . '/' . $post['slug' . $key_suffix] . "?utm_src=rss",
+            'published_at' => $post['published_at'],
+            'category' => $post['category' . $key_suffix],
+            'description' => $post['excerpt' . $key_suffix],
+        ];
+    }
+
+    /**
+     * Get the XML for a single entry by array.
+     *
+     * Must have keys for post array: title, link (also used as guid), published_at, category, link, description,
+     *                                is_published (optional bool).
+     *
+     * @param array $post the post array.
+     *
+     * @return string the entry XML as formatted string.
+     */
+    public static function getEntryXml(array $post): string
     {
         return "<item>
-<title>" . $post['title' . $key_suffix] . "</title>
-<guid>" . C::Config()->get('blog:rss.blog_base_url') . '/' . $post['slug' . $key_suffix] . "?utm_src=rss</guid>
+<title>" . $post['title'] . "</title>
+<guid>" . $post['link'] . "</guid>
 <pubDate>" . Carbon::parse($post['published_at'])->toRfc2822String() . "</pubDate>
-<category>" . $post['category' . $key_suffix] . "</category>
-<link>" . C::Config()->get('blog:rss.blog_base_url') . '/' . $post['slug' . $key_suffix] . "?utm_src=rss</link>
-<description>" . $post['excerpt' . $key_suffix] . "</description>
+<category>" . $post['category'] . "</category>
+<link>" . $post['link'] . "</link>
+<description>" . $post['description'] . "</description>
 </item>\n";
     }
 }
